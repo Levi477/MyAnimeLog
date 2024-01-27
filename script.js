b0b3eed12e0cffda213f851463525995071d907f
Deep <deepgajjar54@gmail.com>
2024-01-27 19:18:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function h3(){
-    document.getElementById("service").innerHTML = "Hey I told you NOT TO CLICK ON BUTTON but you did :( FUCK YOU"
+    document.getElementById("service").innerHTML = "Hey I told you NOT TO CLICK ON BUTTON but you did :( F*** YOU"
 }
